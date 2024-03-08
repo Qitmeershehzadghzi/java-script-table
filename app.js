2db237d7cdd4a55ac919611f7ecf94811f335062
Qitmeershehzad <qitmeershehzad@gmail.com>
2024-03-08 21:02:38 +0500

    java script table

---
+++ app.js
@@ -0,0 +1,13 @@
+let table = prompt ("please enter a table.")
+
+let count =1;
+document.write(table + " x " + count + " = " + table*count + "<br />")          
+document.write(table + " x " + ++count + " = " + table*count + "<br />")
+document.write(table + " x " + ++count + " = " + table*count + "<br />")
+document.write(table + " x " + ++count + " = " + table*count + "<br />")
+document.write(table + " x " + ++count + " = " + table*count + "<br />")
+document.write(table + " x " + ++count + " = " + table*count + "<br />")
+document.write(table + " x " + ++count + " = " + table*count + "<br />")
+document.write(table + " x " + ++count + " = " + table*count + "<br />")
+document.write(table + " x " + ++count + " = " + table*count + "<br>")
+document.write(table + " x " + ++count + " = " + table*count + "<br />")
